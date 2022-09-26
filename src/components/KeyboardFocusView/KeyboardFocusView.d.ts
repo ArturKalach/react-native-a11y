e5a9b4b30693ee4bd8022df189325bb18cb2fa0e
Artur Kalach <artur.kalach.andreevich@gmail.com>
2022-09-26 20:59:04 +0200

    fix(keyboard focus view): fix types

--- src/components/KeyboardFocusView/KeyboardFocusView.d.ts
+++ src/components/KeyboardFocusView/KeyboardFocusView.d.ts
@@ -1,7 +1,7 @@
-import { View, ViewProps } from "react-native";
+import { View } from "react-native";
 
-import { KeyboardFocusViewProps } from "./RCA11yFocusWrapper";
+import { FocusWrapperProps } from "./RCA11yFocusWrapper";
 
 declare const KeyboardFocusView: React.ForwardRefExoticComponent<
-  ViewProps & KeyboardFocusViewProps & React.RefAttributes<View>
+  FocusWrapperProps & React.RefAttributes<View>
 >;
