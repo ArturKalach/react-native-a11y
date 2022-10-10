c82e60806295c3b63957a67f17ae18447bdb7a17
Artur Kalach <artur.kalach.andreevich@gmail.com>
2022-10-10 13:51:01 +0200

    feat(swift to objc): migrate swift to objc

---
+++ ios/Components/RCA11yFocusWrapperManager/RCA11yFocusWrapperManager.h
@@ -0,0 +1,16 @@
+//
+//  RCA11yFocusWrapperManager.h
+//  A11y
+//
+//  Created by Artur Kalach on 07.10.2022.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+#ifndef RCA11yFocusWrapperManager_h
+#define RCA11yFocusWrapperManager_h
+
+#import <React/RCTViewManager.h>
+@interface RCA11yFocusWrapperManager : RCTViewManager
+@end
+
+#endif /* RCA11yFocusWrapperManager_h */
