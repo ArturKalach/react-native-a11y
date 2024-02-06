1c84a597b2d65bc2284d3c2f572c09b7c0b820d6
Boaz Poolman <boaz_skater@hotmail.com>
2024-02-06 09:56:32 +0100

    fix: add missing build import

--- android/src/main/java/com/reactnativea11y/services/KeyboardService.java
+++ android/src/main/java/com/reactnativea11y/services/KeyboardService.java
@@ -14,6 +14,7 @@ import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.View;
+import android.os.Build;
 
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
